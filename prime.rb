34f9b4f52b8e70bae70c1227abf4e0c8f466eae2
Sam B <s77broz@aol.com>
2018-10-10 03:26:36 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
 # Add  code here!
+def prime?(num)
+  return false unless num > 1
+  return true if num == 2
+  isP = true
+  for i in 2...num do
+    val = num / i.to_f
+    if val == val.floor
+      isP = false
+      break
+    end
+  end
+  isP
+end
+
+prime?(3)
