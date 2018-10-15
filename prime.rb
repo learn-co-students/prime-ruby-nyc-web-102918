4a4219620660c9aead5a150f8842d322835b29e5
Matthew Migliore <mmigliore311@gmail.com>
2018-10-15 14:13:04 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,19 @@
 # Add  code here!
+require "pry"
+
+def prime?(integer)
+  test_range = 1..integer
+  numbers_array = test_range.to_a
+  divisible_integer_amount = 0
+
+  numbers_array.each do |number|
+
+    if (integer.to_f/number) == (integer/number)
+      divisible_integer_amount += 1 
+    end 
+
+  end
+  
+  divisible_integer_amount == 2 ? true : false
+  
+end
