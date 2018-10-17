2a1e46449636f9af7ed768cdf971a5700493d6a5
Simon Lee <simonlee0306@gmail.com>
2018-10-17 20:29:02 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
 # Add  code here!
+def prime?(num)
+  x = 2
+#  if num == 0 || num == 1
+#    return false
+#  end
+  if num < 2
+    return false
+  end
+  while x < num
+    if num % x == 0
+      return false
+    end
+    x += 1
+  end
+  return true
+end
