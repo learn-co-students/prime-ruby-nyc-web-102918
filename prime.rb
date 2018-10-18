3633dcdb74e110b3221f1127c87b7d5619118d0b
Ji S <j2yshin@gmail.com>
2018-10-18 21:40:57 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
 # Add  code here!
+def prime?(integer)
+  if integer < 2
+    return false
+  else
+    new_array = (2..(integer - 1)).to_a 
+    new_array.each do |i| 
+      if integer % i == 0
+        return false
+      end
+    end
+  end
+  true
+end
+
