0b1d1d287b43e9075dce1f801b710fc53a56087f
Adam Giegel <adam.giegel@gmail.com>
2018-10-23 17:14:04 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
-# Add  code here!
+def prime?(num)
+  if num is_a?Integer
+    if num % 2 ==  0 && num !=2
+      return false
+    elsif num % 3 == 0 
+      return false
+    elsif num % 5 == 0  
+      return false
+  return false
+else return true
+end
+end
+end
